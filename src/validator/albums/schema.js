7d7dc9d1fde7f2fcf8d151052718e8c6cfa0314d
enjaytarigan <tariganagc@gmail.com>
2022-01-04 21:05:00 +0700

    added max value to property 'year' in album schema

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,8 +1,14 @@
 const Joi = require('joi');
 
+const year = new Date().getFullYear();
 const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  year: Joi.number().integer().min(0).required(),
+  year: Joi
+    .number()
+    .integer()
+    .min(0)
+    .max(year)
+    .required(),
 });
 
 module.exports = { AlbumPayloadSchema };
